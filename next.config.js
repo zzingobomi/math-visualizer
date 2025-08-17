730f8b175c18e1b4f0a795f3cb07ada159e43843
zzingo <zzingo5@gmail.com>
2025-08-17 11:26:11 +0900

    fix: next 14 버전 turbo 적용

--- next.config.js
+++ next.config.js
@@ -1,23 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   transpilePackages: ["three", "@react-three/fiber", "@react-three/drei"],
-  webpack: (config) => {
-    config.externals = config.externals || [];
-    config.externals.push({
-      canvas: "canvas",
-    });
-
-    // Three.js 관련 설정
-    config.module.rules.push({
-      test: /\.(glsl|vs|fs|vert|frag)$/,
-      exclude: /node_modules/,
-      use: ["raw-loader", "glslify-loader"],
-    });
-
-    return config;
+  experimental: {
+    turbo: {
+      rules: {
+        "*.glsl": {
+          loaders: ["raw-loader", "glslify-loader"],
+          as: "*.js",
+        },
+        "*.vs": {
+          loaders: ["raw-loader", "glslify-loader"],
+          as: "*.js",
+        },
+        "*.fs": {
+          loaders: ["raw-loader", "glslify-loader"],
+          as: "*.js",
+        },
+        "*.vert": {
+          loaders: ["raw-loader", "glslify-loader"],
+          as: "*.js",
+        },
+        "*.frag": {
+          loaders: ["raw-loader", "glslify-loader"],
+          as: "*.js",
+        },
+      },
+    },
   },
 };
 
